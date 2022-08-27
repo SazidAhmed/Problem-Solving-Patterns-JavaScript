74eeeb371c90b2ee36a5619b30b95a79a8b3a5d2
sazidahmed <sazidahmed.official@gmail.com>
2022-08-27 21:24:07 +0600

    pure recursion added

--- Patterns/Recursion/colllectOdds.js
+++ Patterns/Recursion/colllectOdds.js
@@ -1,17 +1,35 @@
+// function collectOdds(arr){
+//     let result = [];
+
+//     function helper(helperInput){
+//         if(helperInput.length ===0 ){
+//             return;
+//         }
+
+//         if(helperInput[0] % 2 !==0){
+//             result.push(helperInput.slice(1))
+//         }
+
+//         helper(arr)
+//         console.log(result)
+//         return result;
+//     }
+// }
+
+
+//pure recursion
 function collectOdds(arr){
-    let result = [];
+    let newArr = [];
 
-    function helper(helperInput){
-        if(helperInput.length ===0 ){
-            return;
-        }
-
-        if(helperInput[0] % 2 !==0){
-            result.push(helperInput.slice(1))
-        }
-
-        helper(arr)
-        console.log(result)
-        return result;
+    if(arr.length ===0){
+        return newArr;
     }
+
+    if(arr[0] % 2 == 0){
+        newArr.push(arr[0]);
+    }
+
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    console.log(newArr)
+    return newArr;
 }
