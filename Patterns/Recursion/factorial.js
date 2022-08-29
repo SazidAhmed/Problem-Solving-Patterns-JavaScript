52f2180bb0cb4fe81f59168292f30e62cb738114
sazidahmed <sazidahmed.official@gmail.com>
2022-08-29 23:36:42 +0600

    product of array

--- Patterns/Recursion/factorial.js
+++ Patterns/Recursion/factorial.js
@@ -16,6 +16,16 @@ function factorial(num){
     return num * factorial(num-1)
 }
 
-factorial(3)
+// factorial(3)
 
 // 3 * 2 * 1 = 6
+
+// PRODUCT OF ARRAY SOLUTION
+function productOfArray(arr) {
+    if(arr.length === 0) {
+        return 1;
+    }
+    return arr[0] * productOfArray(arr.slice(1));
+}
+
+productOfArray([1,2,3,4])
