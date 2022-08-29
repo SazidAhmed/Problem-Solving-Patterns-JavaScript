84bb7825468fe2cc708ceaea8661209cdb59480b
sazidahmed <sazidahmed.official@gmail.com>
2022-08-29 23:43:56 +0600

    recursiveRange added

--- Patterns/Recursion/common.js
+++ Patterns/Recursion/common.js
@@ -14,4 +14,28 @@ function factorial(x){
     return x * factorial(x-1);
 }
 
-console.log(factorial(3))
+// console.log(factorial(3))
+
+// PRODUCT OF ARRAY SOLUTION
+function productOfArray(arr) {
+    if(arr.length === 0) {
+        return 1;
+    }
+    return arr[0] * productOfArray(arr.slice(1));
+}
+
+console.log(productOfArray([1,2,4]))
+
+// RECURSIVE RANGE SOLUTION
+function recursiveRange(x){
+    if (x === 0 ) return 0;
+    return x + recursiveRange(x-1);
+}
+
+console.log(recursiveRange(5))
+
+// return 5 + recursiveRange(4);
+//             return 4 + recursiveRange(3);
+//                         return 3 + recursiveRange(2);
+//                                     return 2 + recursiveRange(1);
+//                                                 return 1 + recursiveRange(0);
